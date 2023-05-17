1caa7432c116a61703cb6346ed9f41da19344cac
Juan Pedro Bastias <juan@escolasorvete.com.br>
2023-05-17 14:24:53 -0300

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-    env: {
-        "BASE_URL": "http://localhost:3000/",
-        "MONGODB_URL": "mongodb+srv://almacen_on_line:DdakaJtT8U592CGC@cluster0.krvtd.mongodb.net/amlacen_on_line?retryWrites=true&w=majority",
-        "ACCESS_TOKEN_SECRET": "Z>`u%zX`c.LLRhA?Ks>^+]DRB<2LwK%Q';5b)(VT3D*t&aEw",
-        "REFRESH_TOKEN_SECRET": "K)R!9z8a?rp'QN(^;4B[qybEJ4xxL)LjApyqk_&bYTHwhz9_*E,QyZ/(<tFW=Cj6_Y4J.bSk</E",
-        "CLOUD_UPDATE_PRESET": "almacen_on_line",
-        "CLOUD_NAME": "almacen-on-line",
-        "CLOUD_API": "https://api.cloudinary.com/v1_1/amlacen-on-line/image/upload",
-    }
-}
-  
+  env: {
+    BASE_URL: "https://www.access.com.ar",
+    MONGODB_URL: "mongodb://localhost:27017/accessDB",
+    ACCESS_TOKEN_SECRET: "Z>`u%zX`c.LLRhA?Ks>^+]DRB<2LwK%Q';5b)(VT3D*t&aEw",
+    REFRESH_TOKEN_SECRET:
+      "K)R!9z8a?rp'QN(^;4B[qybEJ4xxL)LjApyqk_&bYTHwhz9_*E,QyZ/(<tFW=Cj6_Y4J.bSk</E",
+    CLOUD_UPDATE_PRESET: "almacen_on_line",
+    CLOUD_NAME: "almacen-on-line",
+    CLOUD_API: "https://api.cloudinary.com/v1_1/amlacen-on-line/image/upload",
+    ACCESS_TOKEN_MP:
+      "APP_USR-8620031361315709-052115-be4bb5cb5110198badc46f49ee6ef79e-1127950295",
+  },
+};
